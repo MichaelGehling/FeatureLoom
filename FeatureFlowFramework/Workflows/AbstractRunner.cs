9b000ab1e202dc2d6ffc8df216c88c075dc1bbec
Michael Gehling <michaelgehling1@gmail.com>
2019-12-30 21:01:08 +0100

    Added executionInfo forwarder to runners and added abstract runner to reduce redundant code.

---
+++ FeatureFlowFramework/Workflows/AbstractRunner.cs
@@ -0,0 +1,79 @@
+﻿using FeatureFlowFramework.DataFlows;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FeatureFlowFramework.Workflows
+{
+    public abstract class AbstractRunner : IWorkflowRunner
+    {
+        protected List<IWorkflowControls> runningWorkflows = new List<IWorkflowControls>();
+        protected readonly IStepExecutionController executionController = new DefaultStepExecutionController();
+        protected Forwarder executionInfoForwarder = null;
+
+        public IEnumerable<IWorkflowControls> RunningWorkflows
+        {
+            get
+            {
+                lock (runningWorkflows)
+                {
+                    return runningWorkflows.ToArray();
+                }
+            }
+        }
+
+        public IDataFlowSource ExecutionInfoSource
+        {
+            get
+            {
+                if (executionInfoForwarder == null)
+                {
+                    executionInfoForwarder = new Forwarder();
+                    lock (runningWorkflows)
+                    {
+                        foreach (var workflow in runningWorkflows)
+                        {
+                            workflow.ExecutionInfoSource.ConnectTo(executionInfoForwarder);
+                        }
+                    }
+
+                }
+                return executionInfoForwarder;
+            }
+        }
+
+        public Task PauseAllWorkflows()
+        {
+            List<Task> tasks = new List<Task>();
+            lock (runningWorkflows)
+            {
+                foreach (var wf in runningWorkflows)
+                {
+                    wf.RequestPause();
+                    tasks.Add(wf.WaitUntilStopsRunningAsync());
+                }
+            }
+            return Task.WhenAll(tasks.ToArray());
+        }
+
+        protected void RemoveFromRunningWorkflows(IWorkflowControls workflow)
+        {
+            lock (runningWorkflows)
+            {
+                runningWorkflows.Remove(workflow);
+                if (executionInfoForwarder != null) workflow.ExecutionInfoSource.DisconnectFrom(executionInfoForwarder);
+            }
+        }
+
+        protected void AddToRunningWorkflows(IWorkflowControls workflow)
+        {
+            lock (runningWorkflows)
+            {
+                runningWorkflows.Add(workflow);
+                if (executionInfoForwarder != null) workflow.ExecutionInfoSource.ConnectTo(executionInfoForwarder);
+            }
+        }
+
+        public abstract void Run(IWorkflowControls workflow);
+    }
+}
--- FeatureFlowFramework/Workflows/ThreadPoolRunner.cs
+++ FeatureFlowFramework/Workflows/ThreadPoolRunner.cs
@@ -6,31 +6,13 @@ using System.Threading.Tasks;
 
 namespace FeatureFlowFramework.Workflows
 {
-    public class ThreadPoolRunner : IWorkflowRunner
-    {
-        private List<IWorkflowControls> runningWorkflows = new List<IWorkflowControls>();
-        private readonly IStepExecutionController executionController = new DefaultStepExecutionController();
-
-        public IEnumerable<IWorkflowControls> RunningWorkflows
-        {
-            get
-            {
-                lock(runningWorkflows)
-                {
-                    return runningWorkflows.ToArray();
-                }
-            }
-        }
-
-        public void Run(IWorkflowControls workflow)
-        {
-            lock(runningWorkflows)
-            {
-                runningWorkflows.Add(workflow);
-            }
-
+    public class ThreadPoolRunner : AbstractRunner
+    {  
+        public override void Run(IWorkflowControls workflow)
+        {            
             Task.Run(() =>
             {
+                AddToRunningWorkflows(workflow);
                 try
                 {
                     while(workflow.ExecuteNextStep(executionController)) ;
@@ -41,23 +23,9 @@ namespace FeatureFlowFramework.Workflows
                 }
                 finally
                 {
-                    lock(runningWorkflows)
-                    {
-                        runningWorkflows.Remove(workflow);
-                    }
+                    RemoveFromRunningWorkflows(workflow);
                 }
             });         
-        }
-
-        public Task PauseAllWorkflows()
-        {
-            List<Task> tasks = new List<Task>();
-            foreach (var wf in RunningWorkflows)
-            {
-                wf.RequestPause();
-                tasks.Add(wf.WaitUntilStopsRunningAsync());
-            }
-            return Task.WhenAll(tasks.ToArray());
         }
     }
 }
