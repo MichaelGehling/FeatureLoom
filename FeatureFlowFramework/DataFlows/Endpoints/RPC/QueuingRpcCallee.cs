6e91ec5e6701ddde35f59b43ff3b47159b84cfcf
Michael Gehling <michaelgehling1@gmail.com>
2020-01-02 17:18:01 +0100

    WaitHandle added to QueuingRpcCallee

--- FeatureFlowFramework/DataFlows/Endpoints/RPC/QueuingRpcCallee.cs
+++ FeatureFlowFramework/DataFlows/Endpoints/RPC/QueuingRpcCallee.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FeatureFlowFramework.Helper;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -23,5 +24,7 @@ namespace FeatureFlowFramework.DataFlows.RPC
                 base.Post(request);
             }
         }
+
+        public IAsyncWaitHandle WaitHandle => queue.WaitHandle;
     }
 }
