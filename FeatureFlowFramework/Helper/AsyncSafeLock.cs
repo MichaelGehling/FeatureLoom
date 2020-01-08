4c9a79ac76bc87b6572731c769a834551349649d
bw1gehm0 <michael.gehling@siemens.com>
2020-01-08 15:55:54 +0100

    AsyncSafeLock using TaskCompletionSource lazily.

--- FeatureFlowFramework/Helper/AsyncSafeLock.cs
+++ FeatureFlowFramework/Helper/AsyncSafeLock.cs
@@ -32,13 +32,24 @@ namespace FeatureFlowFramework.Helper
             return objLock.ForWritingAsync();
         }*/
 
-        volatile int lockId = NO_LOCKID;
-        volatile int lockIdCounter = NO_LOCKID;
-        AsyncManualResetEvent waitingEvent = new AsyncManualResetEvent();
-
         const int NO_LOCKID = 0;
 
-        [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
+        /// <summary>
+        /// Multiple read-locks are allowed in parallel while write-locks are always exclusive.
+        /// A lockId larger than NO_LOCKID (0) implies a write-lock, while a lockId smaller than NO_LOCKID implies a read-lock.
+        /// When entering a read-lock, the lockId is decreased and increased when leaving a read-lock.
+        /// When entering a write-lock, a positive lockId (greater than NO_LOCK) is set and set back to NO_LOCK when the write-lock is left.
+        /// </summary>
+        volatile int lockId = NO_LOCKID;
+
+        /// <summary>
+        /// Used to generate write lock Ids. To generate a new id, it is incremented. 
+        /// When exceeding the maximum value it is reset to NO_LOCKID.
+        /// </summary>
+        volatile int lockIdCounter = NO_LOCKID;
+        //AsyncManualResetEvent waitingEvent = new AsyncManualResetEvent();
+        TaskCompletionSource<bool> tcs;
+
         public IDisposable ForReading()
         {
             var newLockId = 0;
@@ -47,46 +58,64 @@ namespace FeatureFlowFramework.Helper
             {
                 if(lockId > NO_LOCKID)
                 {
-                    waitingEvent.Wait();
+                    //waitingEvent.Wait();
+                    if(tcs == null) Interlocked.CompareExchange(ref tcs, new TaskCompletionSource<bool>(), null);
+                    tcs?.Task.Wait();
                 }
                 currentLockId = lockId;
                 newLockId = currentLockId - 1;
             }
             while(currentLockId > NO_LOCKID || currentLockId != Interlocked.CompareExchange(ref lockId, newLockId, currentLockId));
-            waitingEvent.Reset();
 
-            if(currentLockId > NO_LOCKID)
-            {
-                Console.WriteLine("RL" + currentLockId + "/" + newLockId + " ");
-            }
+            //waitingEvent.Reset();
+            ResetTcs();
 
             return new ReadLock(this);
+        }
+
+        private void ResetTcs()
+        {
+            bool ok = true;
+            do
+            {
+                ok = true;
+                var currentTCS = tcs;
+                if(currentTCS != null && currentTCS.Task.IsCompleted) ok = currentTCS == Interlocked.CompareExchange(ref tcs, new TaskCompletionSource<bool>(), currentTCS);
+            }
+            while(!ok);
         }
 
         public async Task<IDisposable> ForReadingAsync()
         {
             var newLockId = 0;
+            var currentLockId = 0;
             do
             {
                 if(lockId > NO_LOCKID)
                 {
-                    await waitingEvent.WaitingTask;
+                    //await waitingEvent.WaitingTask;
+                    if(tcs == null) Interlocked.CompareExchange(ref tcs, new TaskCompletionSource<bool>(), null);
+                    await tcs?.Task;
                 }
-                var currentLockId = lockId;
+                currentLockId = lockId;
                 newLockId = currentLockId - 1;
-                Interlocked.CompareExchange(ref lockId, newLockId, currentLockId);
             }
-            while(lockId != newLockId);
-            waitingEvent.Reset();
+            while(currentLockId > NO_LOCKID || currentLockId != Interlocked.CompareExchange(ref lockId, newLockId, currentLockId));
+
+            //waitingEvent.Reset();
+            ResetTcs();
 
             return new ReadLock(this);
         }
 
-        [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
         public IDisposable ForWriting()
         {
             var newLockId = Interlocked.Increment(ref lockIdCounter);
-            while(newLockId <= NO_LOCKID) newLockId = Interlocked.Increment(ref lockIdCounter);
+            while(newLockId <= NO_LOCKID)
+            {
+                Interlocked.CompareExchange(ref lockIdCounter, NO_LOCKID, newLockId);
+                newLockId = Interlocked.Increment(ref lockIdCounter);
+            }
 
             var currentLockId = 0;
             do
@@ -94,74 +123,64 @@ namespace FeatureFlowFramework.Helper
                 currentLockId = lockId;
                 if(currentLockId != NO_LOCKID)
                 {
-                    waitingEvent.Wait();
+                    //waitingEvent.Wait();
+                    if(tcs == null) Interlocked.CompareExchange(ref tcs, new TaskCompletionSource<bool>(), null);
+                    tcs?.Task.Wait();
                 }
             }
             while(currentLockId != NO_LOCKID || currentLockId != Interlocked.CompareExchange(ref lockId, newLockId, NO_LOCKID));
-            waitingEvent.Reset();
 
-            if(currentLockId != NO_LOCKID)
-            {
-                Console.WriteLine("WL" + currentLockId + "/" + newLockId + " ");
-            }
+            //waitingEvent.Reset();
+            ResetTcs();
 
-            return new WriteLock(this, newLockId);
+            return new WriteLock(this);
         }
 
         public async Task<IDisposable> ForWritingAsync()
         {
             var newLockId = Interlocked.Increment(ref lockIdCounter);
-            while(newLockId == NO_LOCKID) newLockId = Interlocked.Increment(ref lockIdCounter);
+            while(newLockId <= NO_LOCKID)
+            {
+                Interlocked.CompareExchange(ref lockIdCounter, NO_LOCKID, newLockId);
+                newLockId = Interlocked.Increment(ref lockIdCounter);
+            }
+
+            var currentLockId = 0;
             do
             {
-                if(lockId != NO_LOCKID)
+                currentLockId = lockId;
+                if(currentLockId != NO_LOCKID)
                 {
-                    await waitingEvent.WaitingTask;
+                    //await waitingEvent.WaitingTask;
+                    if(tcs == null) Interlocked.CompareExchange(ref tcs, new TaskCompletionSource<bool>(), null);
+                    await tcs?.Task;
                 }
-                Interlocked.CompareExchange(ref lockId, newLockId, NO_LOCKID);
             }
-            while(lockId != newLockId);
-            waitingEvent.Reset();
+            while(currentLockId != NO_LOCKID || currentLockId != Interlocked.CompareExchange(ref lockId, newLockId, NO_LOCKID));
 
-            return new WriteLock(this, newLockId);
+            //waitingEvent.Reset();
+            ResetTcs();
+
+            return new WriteLock(this);
         }
 
         private struct ReadLock : IDisposable
         {
             AsyncSafeLock safeLock;
 
-            [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
             public ReadLock(AsyncSafeLock safeLock)
             {
                 this.safeLock = safeLock;
             }
 
-            [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
             public void Dispose()
             {
-                /*var newLockId = 0;
-                var currentLockId = 0;
-                do
-                { 
-                    currentLockId = safeLock.lockId;
-                    newLockId = currentLockId + 1;
-                    if (newLockId > NO_LOCKID)
-                    {
-                        Console.WriteLine("R*" + currentLockId + "/" + newLockId + "/" + safeLock.lockId + " ");
-                        continue;
-                    }
-                }
-                while(currentLockId != Interlocked.CompareExchange(ref safeLock.lockId, newLockId, currentLockId));
-                */
                 var newLockId = Interlocked.Increment(ref safeLock.lockId);
 
                 if (NO_LOCKID == newLockId)
                 {
-                    safeLock.waitingEvent.Set();
-                }
-                else if (newLockId > NO_LOCKID)
-                {
-                    Console.WriteLine("RU"+ newLockId + "/" + safeLock.lockId + " ");
+                    //safeLock.waitingEvent.Set();
+                    safeLock.tcs?.TrySetResult(true);
                 }
             }
         }
@@ -169,29 +188,17 @@ namespace FeatureFlowFramework.Helper
         private struct WriteLock : IDisposable
         {
             AsyncSafeLock safeLock;
-            int myLockId;
 
-            [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
-            public WriteLock(AsyncSafeLock safeLock, int myLockId)
+            public WriteLock(AsyncSafeLock safeLock)
             {
                 this.safeLock = safeLock;
-                this.myLockId = myLockId;
             }
 
-            [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
             public void Dispose()
             {
-                var currentLockId = safeLock.lockId;
-                if(currentLockId == myLockId)
-                {
-                    //safeLock.lockId = NO_LOCKID;
-                    Interlocked.Exchange(ref safeLock.lockId, NO_LOCKID);
-                    safeLock.waitingEvent.Set();
-                }
-                else
-                {
-                    Console.WriteLine("WU" + currentLockId + "/" + myLockId + " ");
-                }
+                safeLock.lockId = NO_LOCKID;
+                //safeLock.waitingEvent.Set();
+                safeLock.tcs?.TrySetResult(true);
             }
         }
     }
