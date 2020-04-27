6eed9e628f808e72fc6df9d5acb0dba769c55d6c
MichaelGehling <michaelgehling1@gmail.com>
2020-04-27 13:27:01 +0200

    1. Added Substring Extension method 2. Added A method for trying function calls possibly throwing an exception 3. Moved ByteArray extension method to proper class

---
+++ FeatureFlowFramework/Helper/ByteArrayExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FeatureFlowFramework.Helper
+{
+    public static class ByteArrayExtensions
+    {
+        public static int FindPatternPos(this byte[] buffer, byte[] pattern)
+        {
+            return buffer.FindPatternPos(0, buffer.Length, pattern);
+        }
+    }
+}
---
+++ FeatureFlowFramework/Helper/Helpers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FeatureFlowFramework.Helper
+{
+    public static class Helpers
+    {
+        public static bool Try<T>(Func<T> function, out T result)
+        {
+            try
+            {
+                result = function();
+                return true;
+            }
+            catch
+            {
+                result = default;
+                return false;
+            }
+        }
+
+        public static bool Try(Action action)
+        {
+            try
+            {
+                action();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
