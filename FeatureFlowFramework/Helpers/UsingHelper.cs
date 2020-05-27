36ed75407a8603e9eac2da9f6f1afd60b50d0650
MichaelGehling <michaelgehling1@gmail.com>
2020-05-27 08:31:44 +0200

    1. MetaData fixed by removing generic class and using object + generic medthods instead 2. fixed Substring extension 3. UsingHelper added

---
+++ FeatureFlowFramework/Helpers/UsingHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FeatureFlowFramework.Helpers
+{
+    public class UsingHelper : IDisposable
+    {
+        Action after;
+
+        public UsingHelper(Action before, Action after)
+        {
+            before?.Invoke();
+            this.after = after;
+        }
+
+        public void Dispose()
+        {
+            after();
+        }        
+    }
+}
--- FeatureFlowFramework/Services/MetaData/MetaDataExtensions.cs
+++ FeatureFlowFramework/Services/MetaData/MetaDataExtensions.cs
@@ -4,12 +4,12 @@ namespace FeatureFlowFramework.Services.MetaData
 {
     public static class MetaDataExtensions
     {
-        public static void SetMetaData<T, D>(this T obj, string key, D data) where T : class => MetaData<T>.SetMetaData(obj, key, data);
+        public static void SetMetaData<T, D>(this T obj, string key, D data) where T : class => MetaData.SetMetaData(obj, key, data);
 
-        public static bool TryGetMetaData<T, D>(this T obj, string key, out D data) where T : class => MetaData<T>.TryGetMetaData(obj, key, out data);        
+        public static bool TryGetMetaData<T, D>(this T obj, string key, out D data) where T : class => MetaData.TryGetMetaData(obj, key, out data);        
 
-        public static ObjectHandle GetHandle<T>(this T obj) where T:class => MetaData<T>.GetHandle(obj);
+        public static ObjectHandle GetHandle<T>(this T obj) where T:class => MetaData.GetHandle(obj);
 
-        public static FeatureLock GetLock<T>(this T obj) where T:class => MetaData<T>.GetLock(obj);        
+        public static FeatureLock GetLock<T>(this T obj) where T:class => MetaData.GetLock(obj);        
     }
 }
