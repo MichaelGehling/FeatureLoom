b3009efbb0e03758641c818dcf21a6c9e8756148
MichaelGehling <michaelgehling1@gmail.com>
2020-06-15 13:18:42 +0200

    MAde tests more robust

--- FeatureFlowFramework.Tests/DataFlows/Endpoints/TcpTests.cs
+++ FeatureFlowFramework.Tests/DataFlows/Endpoints/TcpTests.cs
@@ -31,8 +31,8 @@ namespace FeatureFlowFramework.DataFlows.TCP
             serverSender.ConnectTo(server);
             server.ConnectTo(serverReceiver);
 
-            Assert.True(client.ConnectionWaitHandle.Wait(1.Seconds()));
-            Assert.True(server.ConnectionWaitHandle.Wait(1.Seconds()));
+            Assert.True(client.ConnectionWaitHandle.Wait(2.Seconds()));
+            Assert.True(server.ConnectionWaitHandle.Wait(2.Seconds()));
             Assert.True(client.IsConnectedToServer);
             Assert.Equal(1, server.CountConnectedClients);
 
@@ -72,8 +72,8 @@ namespace FeatureFlowFramework.DataFlows.TCP
             serverSender.ConnectTo(server);
             server.ConnectTo(serverReceiver);
 
-            Assert.True(client.ConnectionWaitHandle.Wait(1.Seconds()));
-            Assert.True(server.ConnectionWaitHandle.Wait(1.Seconds()));
+            Assert.True(client.ConnectionWaitHandle.Wait(2.Seconds()));
+            Assert.True(server.ConnectionWaitHandle.Wait(2.Seconds()));
             Assert.True(client.IsConnectedToServer);
             Assert.Equal(1, server.CountConnectedClients);
 
