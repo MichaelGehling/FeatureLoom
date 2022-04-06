18c53a4af58694c47d30a5cad175496a18ee017c
Michael Gehling <Michael.Gehling@siemens.com>
2022-04-06 19:28:23 +0200

    Working on new Service class, but not yet finished

---
+++ FeatureLoom.Core/Services/Service.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace FeatureLoom.Services
+{
+    public static class Service<T> where T:class
+    {
+        private static ServiceInstanceContainer instanceContainer;
+
+        public static void Init(Func<T> createServiceAction)
+        {
+            if (instanceContainer == null) Interlocked.CompareExchange(ref instanceContainer, ServiceInstanceContainer.Create(createServiceAction), null);
+        }
+
+        public static void Init<DFLT>() where DFLT : T, new()
+        {
+            if (instanceContainer == null) Interlocked.CompareExchange(ref instanceContainer, ServiceInstanceContainer.Create<DFLT>(), null);
+        }
+
+        public static T GetInstance<DFLT>() where DFLT : T, new()
+        {
+            if (instanceContainer == null) Interlocked.CompareExchange(ref instanceContainer, ServiceInstanceContainer.Create<DFLT>(), null);
+            return instanceContainer.Instance;
+        }
+
+        public static void SetInstance<DFLT>(DFLT serviceInstance) where DFLT : T, new()
+        {            
+            if (instanceContainer == null) Interlocked.CompareExchange(ref instanceContainer, ServiceInstanceContainer.Create<DFLT>(serviceInstance), null);
+            instanceContainer.Instance = serviceInstance;
+        }
+
+        public static void CreateLocalServiceInstance<DFLT>(DFLT localServiceInstance) where DFLT : T, new()
+        {
+            if (instanceContainer == null) Interlocked.CompareExchange(ref instanceContainer, ServiceInstanceContainer.Create<DFLT>(localServiceInstance ?? new DFLT()), null);
+            instanceContainer.CreateLocalServiceInstance(localServiceInstance);
+        }
+
+        internal class ServiceInstanceContainer : IServiceInstanceContainer
+        {
+            T globalInstance;
+            AsyncLocal<T> localInstance;
+            Func<T> createInstance;
+
+            private ServiceInstanceContainer()
+            {                
+            }
+
+            public static ServiceInstanceContainer Create<REAL>(T serviceInstance = null) where REAL:T, new()
+            {
+                var container = new ServiceInstanceContainer() { createInstance = () => new REAL(), globalInstance = serviceInstance ?? new REAL() };                
+                ServiceRegistry.RegisterService(container);
+                return container;
+            }
+            public static ServiceInstanceContainer Create(Func<T> createInstance)
+            {
+                var container = new ServiceInstanceContainer() { createInstance = createInstance, globalInstance = createInstance() };
+                ServiceRegistry.RegisterService(container);
+                return container;
+            }
+
+
+            public T Instance
+            {
+                get => localInstance?.Value ?? globalInstance;                    
+                set
+                {
+                    if (localInstance != null) localInstance.Value = value;
+                    else globalInstance = value;
+                }
+            }
+
+            public void CreateLocalServiceInstance(T localServiceInstance)
+            {
+                if (localInstance == null)
+                {
+                    Interlocked.CompareExchange(ref localInstance, new AsyncLocal<T>(), null);
+                }
+                localInstance.Value = localServiceInstance;
+            }
+
+            public void CreateLocalServiceInstance()
+            {
+                if (localInstance == null)
+                {
+                    Interlocked.CompareExchange(ref localInstance, new AsyncLocal<T>(), null);
+                }
+                localInstance.Value = createInstance();
+            }
+
+            public void ClearAllLocalServiceInstances(bool useLocalInstanceAsGlobal)
+            {
+                if (useLocalInstanceAsGlobal) globalInstance = localInstance.Value;
+                localInstance = null;
+            }
+
+            public Type ServiceType => typeof(T);
+            object IServiceInstanceContainer.Instance => Instance;
+        }
+    }    
+}
---
+++ FeatureLoom.Core/Services/ServiceRegistry.cs
@@ -0,0 +1,52 @@
+﻿using FeatureLoom.Synchronization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace FeatureLoom.Services
+{
+    public static class ServiceRegistry
+    {
+        static Dictionary<Type, IServiceInstanceContainer> registry = new Dictionary<Type, IServiceInstanceContainer>();
+        static FeatureLock registryLock = new FeatureLock();        
+
+        internal static void RegisterService(IServiceInstanceContainer service)
+        {
+            using (registryLock.Lock())
+            {
+                registry[service.ServiceType] = service;
+            }
+        }                
+
+        public static IServiceInstanceContainer[] GetLocalServices()
+        {
+            using(registryLock.Lock())
+            {
+                return registry.Values.ToArray();
+            }
+        }
+
+        public static void CreateLocalInstancesForAllServices()
+        {
+            using (registryLock.Lock())
+            {
+                foreach(var service in registry.Values)
+                {
+                    service.CreateLocalServiceInstance();
+                }
+            }
+        }
+
+        public static void ClearAllLocalServiceInstances(bool useLocalInstanceAsGlobal)
+        {
+            using (registryLock.Lock())
+            {
+                foreach (var service in registry.Values)
+                {
+                    service.ClearAllLocalServiceInstances(useLocalInstanceAsGlobal);
+                }
+            }
+        }
+    }
+}
