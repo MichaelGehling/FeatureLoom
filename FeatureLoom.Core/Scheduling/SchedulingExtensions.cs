2a2c668396caa57cb249c122707826e5484665c5
Michael Gehling <michaelgehling1@gmail.com>
2022-07-02 22:13:00 +0200

    Added comment

--- FeatureLoom.Core/Scheduling/SchedulingExtensions.cs
+++ FeatureLoom.Core/Scheduling/SchedulingExtensions.cs
@@ -33,6 +33,14 @@ namespace FeatureLoom.Scheduling
             return await asyncAction();
         }
 
+        /// <summary>
+        /// Creates a new schedule based on a lamda function, adds it to the scheduler and returns it.
+        /// NOTE: The scheduler only keeps a weak reference to the schedule. If the schedule is not kept in another reference, it will be garbage collected.
+        /// </summary>
+        /// <param name="name">The name of the new schedule</param>
+        /// <param name="triggerAction">The function takes the current time as input parameter and returns a tuple with two values:
+        /// 1. If the schedule continues (true) or if it finished (false) and 2. the longest possible wait time when it needs to be triggered again.</param>
+        /// <returns>The created schedule.</returns>
         public static ActionSchedule ScheduleAction(this IScheduler scheduler, string name, Func<DateTime, TimeFrame> triggerAction)
         {
             var schedule = new ActionSchedule(name, triggerAction);
