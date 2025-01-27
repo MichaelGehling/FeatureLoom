17961a24b05397c227e2440bfb3969d7281dd9af
Michael Gehling <michaelgehling1@gmail.com>
2025-01-27 07:50:59 +0100

    Add support for JsonIgnore attribute in JsonSerializer

---
+++ FeatureLoom.Core/Serialization/Annotations.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FeatureLoom.Serialization
+{
+    /*
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+    public class JsonIgnoreAttribute : Attribute
+    {
+    }
+    */
+}
---
+++ FeatureLoom.Core/Serialization/JsonHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FeatureLoom.Serialization;
+
+public static class JsonHelper
+{
+    static FeatureJsonSerializer serializer = new();
+    static FeatureJsonDeserializer deserializer = new();
+
+    public static FeatureJsonSerializer DefaultSerializer => serializer;
+    public static FeatureJsonDeserializer DefaultDeserializer => deserializer;
+}
