473488d62b9956de7c1359490dcfd44d6c3fe448
Michael Gehling <michaelgehling1@gmail.com>
2025-06-01 00:58:14 +0200

    Extended and commented Pool

--- FeatureLoom.Core/Collections/Pool.cs
+++ FeatureLoom.Core/Collections/Pool.cs
@@ -6,10 +6,18 @@ using System.Text;
 
 namespace FeatureLoom.Collections
 {
-    public sealed class Pool<T>  where T : class
+    /// <summary>
+    /// Thread-safe or single-threaded object pool for reusing instances of <typeparamref name="T"/>.
+    /// 
+    /// The pool uses a stack to store reusable objects. When <see cref="Take"/> is called, an object is taken from the pool
+    /// or created using the provided factory if the pool is empty. When <see cref="Return"/> is called, the object is optionally
+    /// reset and returned to the pool if the pool has not reached its maximum size.
+    /// The pool can be cleared with <see cref="Clear"/> and the current count of pooled objects is available via <see cref="Count"/>.
+    /// </summary>
+    public sealed class Pool<T> where T : class
     {
-        Stack<T> stack = new Stack<T>();        
-        FeatureLock myLock;        
+        Stack<T> stack = new Stack<T>();
+        MicroLock myLock;
         Func<T> take;
         Action<T> ret;
 
@@ -17,7 +25,7 @@ namespace FeatureLoom.Collections
         {
             if (threadSafe)
             {
-                myLock = new FeatureLock();
+                myLock = new MicroLock();
                 take = () =>
                 {
                     using (myLock.Lock())
@@ -74,8 +82,31 @@ namespace FeatureLoom.Collections
             }
         }
 
+        /// <summary>
+        /// Takes an object from the pool, or creates a new one if the pool is empty.
+        /// </summary>
         public T Take() => take();
 
+        /// <summary>
+        /// Returns an object to the pool. Optionally resets the object before returning.
+        /// </summary>
+        /// <param name="obj">The object to return to the pool.</param>
         public void Return(T obj) => ret(obj);
+
+        /// <summary>
+        /// Gets the current number of objects in the pool.
+        /// </summary>
+        public int Count => stack.Count;
+
+        /// <summary>
+        /// Removes all objects from the pool.
+        /// </summary>
+        public void Clear()
+        {
+            using (myLock?.Lock())
+            {
+                stack.Clear();
+            }
+        }
     }
 }
