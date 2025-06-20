74689f37d3d98ef6b0e5e00cdcd8b9997ec07316
Michael Gehling <michael.gehling@siemens.com>
2025-06-20 12:37:06 +0200

    Added DelegateEqualityComparer, becasue the recently removed GenericComparer left a functional gap.

---
+++ FeatureLoom.Core/Helpers/DelegateEqualityComparer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FeatureLoom.Helpers;
+
+/// <summary>
+/// Provides a flexible implementation of <see cref="IEqualityComparer{T}"/> by allowing
+/// custom equality and hash code logic to be supplied via delegates.
+/// 
+/// Note: This class serves as a replacement for <c>EqualityComparer&lt;T&gt;.Create</c>,
+/// which is not available in .NET Standard.
+/// </summary>
+/// <typeparam name="T">The type of objects to compare.</typeparam>
+public class DelegateEqualityComparer<T> : IEqualityComparer<T>
+{
+    private readonly Func<T, T, bool> equals;
+    private readonly Func<T, int> getHashCode;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DelegateEqualityComparer{T}"/> class.
+    /// </summary>
+    /// <param name="equals">
+    /// A delegate that defines the method to determine equality between two objects.
+    /// </param>
+    /// <param name="getHashCode">
+    /// A delegate that defines the method to compute the hash code for an object.
+    /// If null, the type's default GetHashCode is used.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown if <paramref name="equals"/> is null.
+    /// </exception>
+    public DelegateEqualityComparer(Func<T, T, bool> equals, Func<T, int> getHashCode = null)
+    {
+        this.equals = equals ?? throw new ArgumentNullException(nameof(equals));
+        this.getHashCode = getHashCode ?? (obj => obj?.GetHashCode() ?? 0);
+    }
+
+    /// <summary>
+    /// Determines whether the specified objects are equal using the provided equality delegate.
+    /// </summary>
+    /// <param name="x">The first object to compare.</param>
+    /// <param name="y">The second object to compare.</param>
+    /// <returns>True if the objects are considered equal; otherwise, false.</returns>
+    public bool Equals(T x, T y) => equals(x, y);
+
+    /// <summary>
+    /// Returns a hash code for the specified object using the provided hash code delegate.
+    /// </summary>
+    /// <param name="obj">The object for which to get a hash code.</param>
+    /// <returns>A hash code for the specified object.</returns>
+    public int GetHashCode(T obj) => getHashCode(obj);
+}
---
+++ FeatureLoom.Tests/Helpers/DelegateEqualityComparerTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using FeatureLoom.Helpers;
+using Xunit;
+
+namespace FeatureLoom.Helpers;
+
+public class DelegateEqualityComparerTests
+{
+    [Fact]
+    public void Equals_ReturnsTrue_ForEqualObjects()
+    {
+        var comparer = new DelegateEqualityComparer<string>(
+            (a, b) => string.Equals(a, b, StringComparison.OrdinalIgnoreCase)
+        );
+
+        Assert.True(comparer.Equals("test", "TEST"));
+    }
+
+    [Fact]
+    public void Equals_ReturnsFalse_ForNonEqualObjects()
+    {
+        var comparer = new DelegateEqualityComparer<int>(
+            (a, b) => a == b
+        );
+
+        Assert.False(comparer.Equals(1, 2));
+    }
+
+    [Fact]
+    public void GetHashCode_UsesCustomDelegate()
+    {
+        var comparer = new DelegateEqualityComparer<string>(
+            (a, b) => a.Length == b.Length,
+            s => s.Length
+        );
+
+        Assert.Equal(comparer.GetHashCode("abc"), comparer.GetHashCode("xyz"));
+        Assert.NotEqual(comparer.GetHashCode("abc"), comparer.GetHashCode("abcd"));
+    }
+
+    [Fact]
+    public void GetHashCode_UsesDefaultIfNotProvided()
+    {
+        var comparer = new DelegateEqualityComparer<string>(
+            (a, b) => a == b
+        );
+
+        string value = "hello";
+        Assert.Equal(value.GetHashCode(), comparer.GetHashCode(value));
+    }
+
+    [Fact]
+    public void ThrowsArgumentNullException_IfEqualsIsNull()
+    {
+        Assert.Throws<ArgumentNullException>(() => new DelegateEqualityComparer<object>(null));
+    }
+
+    [Fact]
+    public void GetHashCode_ReturnsZero_ForNullObject()
+    {
+        var comparer = new DelegateEqualityComparer<string>(
+            (a, b) => a == b
+        );
+
+        Assert.Equal(0, comparer.GetHashCode(null));
+    }
+}
